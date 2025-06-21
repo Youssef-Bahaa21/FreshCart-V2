adeadb02f437ff43b8e5ed033694c111eb709915
Youssefbahaa2025 <kyy98589@gmail.com>
2025-06-22 01:27:56 +0300

    fix: Update deployment configuration for Vercel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideToastr } from 'ngx-toastr';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { headersInterceptor } from './core/interceptors/headers/headers.interceptor';
-import { errorsInterceptor } from './core/interceptors/errors/errors.interceptor';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),provideAnimations(), provideToastr(), provideHttpClient(withFetch() , withInterceptors([headersInterceptor , errorsInterceptor])),provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    provideClientHydration(),
+    provideAnimations(),
+    provideHttpClient(withFetch()),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    })
+  ]
 };
